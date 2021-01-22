f66d1d3a8ecc91fa2807aaed4012e2e1ae42a706
ahmadfaritzip <ahmadfaritzip@gmail.com>
2021-01-22 16:15:10 +0700

    add function checklist

--- script.js
+++ script.js
@@ -18,3 +18,66 @@ function playAudio() {
 } 
 
 // // end play audio
+
+
+// Setting Cookies
+function setCookie(cname,cvalue,exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie(data_session) {
+  var data=getCookie(data_session);
+  if (data != "") {
+    console.log(data);
+  } else {
+     data = prompt("Please enter your name:","");
+     if (data != "" && data != null) {
+       setCookie(data_session, data, 30);
+     }
+  }
+}
+
+
+function getSession(data_session){
+  return sessionStorage.getItem(data_session);
+}
+
+function setSession(key, val){
+  sessionStorage.setItem(key, val);
+}
+
+function checkBrowserSupportSession(data_session){
+  if (typeof(Storage) !== "undefined") {
+    getSession(data_session)
+  } else {
+    alert("Sorry, your browser does not support Web Storage...");
+  }
+}
+
+
+function getlocalStorage(data_local_storage){
+  return localStorage.getItem(data_local_storage);
+}
+
+function setlocalStorage(key, val){
+  localStorage.setItem(key, val);
+}
